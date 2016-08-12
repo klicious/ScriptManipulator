43a5c09fa9442a17d6fa475ec442506e71b4eb6a
klicious <t.y.kwon320@gmail.com>
2016-08-12 10:32:09 +0900

    additional functions
    subject matter

--- src/ConvertScript.java
+++ src/ConvertScript.java
@@ -9,96 +9,77 @@ import java.util.TreeSet;
 
 public class ConvertScript {
 
+	static Map<Integer,Script> subtitle;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		init();
+		String outputPath, inputPath;
 		
-		/*
-		 * Write OUTPUT to OUTPUT_FILE
-		 */
-		PrintWriter writer = null;
-		try{
-			writer = new PrintWriter("Movies_LoveActually.txt", "UTF-8");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		/*
-		 * Read INPUT from INFPUT_FILE
-		 */
-		String line = "";
-		Map<Integer, Script> subtitle = new HashMap<>();
-		try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Kwon\\workspace\\PronounceBetter\\ScriptManipulator\\Love_Actually.txt"))){
-			
-			while( (line = br.readLine()) != null) {
-				//!line.matches(".*[*-].*") && line.length() > 1
-				if(line.length() < 2) continue;
-				
-				if(line.contains("SYNC")) {
-					if(line.contains("&nbsp")) {
-						continue;
-					}
-					line = line.substring(line.indexOf('=') + 1, line.indexOf('>'));
-					//System.out.println(line);
-					
-					int key = Integer.valueOf(line);
-					Script value;
-					if ((line = br.readLine()) != null) {
-						line = line.replaceAll("<.*>", " ").replaceAll("-", " ").replaceAll("\\(.*\\)", "").trim();
-						if (line.length() < 2) continue;
-						if (subtitle.containsKey(key)) {
-							value = subtitle.get(key);
-							value.setEnglish(line);
-							subtitle.put(key, value);
-						} else {
-							value = new Script(line, line);
-							subtitle.put(key, value);							
-							//System.out.println(line);
-						}
-					}
-										
-				}
-					
+		for(int i = 0; i < 16; i++) {
+			int a = i + 1;
+			String episode;
+			if(a < 10) {
+				episode = "0" + a;
+			}else {
+				episode = String.valueOf(a);
 			}
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		for ( Map.Entry<Integer, Script> entry : subtitle.entrySet()) {
-			System.out.println("Value of Key :" + String.valueOf(entry.getKey()));
-			System.out.println("Korean saved :" + entry.getValue().getKorean());
-			System.out.println("English saved : " + entry.getValue().getEnglish());
-			System.out.println(" ============================ ");
+
+			outputPath = "Friends_S01_E" + episode + ".txt";
+			/*
+			 * Write OUTPUT to OUTPUT_FILE
+			 */
+			PrintWriter writer = null;
+			try{
+				writer = new PrintWriter(outputPath, "UTF-8");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			
+			/*
+			 * Read INPUT from INFPUT_FILE
+			 */
+			
+/*			
+			for ( Map.Entry<Integer, Script> entry : subtitle.entrySet()) {
+				System.out.println("Value of Key :" + String.valueOf(entry.getKey()));
+				System.out.println("Korean saved :" + entry.getValue().getKorean());
+				System.out.println("English saved : " + entry.getValue().getEnglish());
+				System.out.println(" ============================ ");
+				
+			}
+			*/
+			SortedSet<Integer> keys = new TreeSet<Integer>(subtitle.keySet());
+			//writer.println("HERE IT STARTS for Love_Actually");
+			for ( Integer ret : keys) {
+				Script entry = subtitle.get(ret);
+				writer.println(entry.getEnglish());
+				writer.println(entry.getKorean());
+			}
+			
+			inputPath = "C:\\Users\\Kwon\\workspace\\PronounceBetter\\ScriptManipulator\\Friends_S01_E" + episode + "_orignial.txt";
+			
+			try (BufferedReader br = new BufferedReader(new FileReader(inputPath))){
+				String line;
+				while( (line = br.readLine()) != null) {
+					//!line.matches(".*[*-].*") && line.length() > 1
+					if(line.length() < 2) continue;
+					if (!(line.substring(0,1).equals("*") || line.substring(0,1).equals("-"))) {
+						writer.println(line.replaceAll("\\[.*\\]", "").replaceAll("\\(.*\\)", "").trim());
+					} else {
+						//System.out.println(line);
+					}
+					
+				}
+				br.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			writer.close();
 		}
 		
-		SortedSet<Integer> keys = new TreeSet<Integer>(subtitle.keySet());
-		writer.println("HERE IT STARTS for Love_Actually");
-		for ( Integer ret : keys) {
-			Script entry = subtitle.get(ret);
-			writer.println(entry.getEnglish());
-			writer.println(entry.getKorean());
-		}
+		
 /*		
-		try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Kwon\\workspace\\PronounceBetter\\ScriptManipulator\\Friends_S01_E01_orignial.txt"))){
-			
-			while( (line = br.readLine()) != null) {
-				//!line.matches(".*[*-].*") && line.length() > 1
-				if(line.length() < 2) continue;
-				if (!(line.substring(0,1).equals("*") || line.substring(0,1).equals("-"))) {
-					writer.println(line.replaceAll("\\[.*\\]", "").replaceAll("\\(.*\\)", "").trim());
-				} else {
-					System.out.println(line);
-				}
-				
-			}
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-*/
-		
-		
 		ArrayList<String> fullScript = new ArrayList<String>();
         ArrayList<Script> theScript = new ArrayList<Script>();
 		
@@ -111,7 +92,7 @@ try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Kwon\\wor
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-writer.close();
+*/
 /*		
 		String english = "", korean = "";
 		boolean flag = true;
@@ -158,6 +139,53 @@ writer.close();
         }
 		System.out.println("너 한글 읽을 줄 아니 ??");
 	}*/
+	}
+	
+	private static void init() {
+		subtitle = new HashMap<>();
+	}
+	
+	private void readLoveActually(){
+		String line = "";
+		
+		subtitle.clear();
+		try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Kwon\\workspace\\PronounceBetter\\ScriptManipulator\\Love_Actually.txt"))){
+			
+			while( (line = br.readLine()) != null) {
+				//!line.matches(".*[*-].*") && line.length() > 1
+				if(line.length() < 2) continue;
+				
+				if(line.contains("SYNC")) {
+					if(line.contains("&nbsp")) {
+						continue;
+					}
+					line = line.substring(line.indexOf('=') + 1, line.indexOf('>'));
+					//System.out.println(line);
+					
+					int key = Integer.valueOf(line);
+					Script value;
+					if ((line = br.readLine()) != null) {
+						line = line.replaceAll("<.*>", " ").replaceAll("-", " ").replaceAll("\\(.*\\)", "").trim();
+						if (line.length() < 2) continue;
+						if (subtitle.containsKey(key)) {
+							value = subtitle.get(key);
+							value.setEnglish(line);
+							subtitle.put(key, value);
+						} else {
+							value = new Script(line, line);
+							subtitle.put(key, value);							
+							//System.out.println(line);
+						}
+					}
+										
+				}
+					
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
 
